03ce30549ac0f0132750bb9d49899a594f20f7a1
Pradeep Reddy Raamana <raamana@gmail.com>
2017-03-03 10:37:24 -0500

    - Enabled methods to allow input of user-defined features. - Updated the readme with better usage notes.

--- psy.py
+++ psy.py
@@ -1,3 +1,5 @@
+#/usr/bin/env python
+
 import sys
 import os
 import nibabel
@@ -5,24 +7,37 @@ import sklearn
 import argparse
 from pyradigm import MLDataset
 
+def not_unspecified( var ):
+    """ Checks for null values of a give variable! """
+
+    return var not in [ 'None', None, '' ]
+
+
 def parse_args():
     """Parser/validator for the cmd line args."""
 
     parser = argparse.ArgumentParser(prog="psy")
 
+    parser.add_argument("-m", "--metadatafile", action="store", dest="metadatafile",
+                        default=None, required=True,
+                        help="Abs path to file containing metadata for subjects to be included for analysis. At the "
+                             "minimum, each subject should have an id per row followed by the class it belongs to. "
+                             "E.g. \n"
+                             "sub001,control\n"
+                             "sub002,control\n"
+                             "sub003,disease\n"
+                             "sub004,disease\n")
+
     parser.add_argument("-f", "--fsdir", action="store", dest="fsdir",
                         default=None,
                         help="Abs. path of SUBJECTS_DIR containing the finished runs of Freesurfer parcellation")
 
-    parser.add_argument("-m", "--metadatafile", action="store", dest="metadatafile",
+    parser.add_argument("-u", "--userdir", action="store", dest="userdir",
                         default=None,
-                        help="Abs path to file containing metadata for subjects to be included for analysis. At the "
-                             "minimum, each subject should have an id per row followed by the class it belongs to. "
-                             "E.g. "
-                             "sub001,control"
-                             "sub002,control"
-                             "sub003,disease"
-                             "sub004,disease")
+                        help="Abs. path to an user's own features."
+                             "This contains a separate folder for each subject (named after its ID in the metadata "
+                             "file) containing a file called features.txt with one number per line. All the subjects "
+                             "must have the number of features (#lines in file)")
 
     parser.add_argument("-o", "--outdir", action="store", dest="outdir",
                         default=None,
@@ -39,30 +54,39 @@ def parse_args():
     except:
         parser.exit(1)
 
-    fsdir = os.path.abspath(options.fsdir)
     metadatafile = os.path.abspath(options.metadatafile)
-    outdir = os.path.abspath(options.outdir)
+    assert os.path.exists(metadatafile), "Given metadata file doesn't exist."
 
-    assert os.path.exists(fsdir), "Given Freesurfer directory doesnt exist."
-    assert os.path.exists(metadatafile), "Given metadata file doesnt exist."
+    if not_unspecified(options.fsdir):
+        fsdir = os.path.abspath(options.fsdir)
+        assert os.path.exists(fsdir), "Given Freesurfer directory doesn't exist."
+        userdir = None
+    elif not_unspecified(options.userdir):
+        fsdir = None
+        userdir = os.path.abspath(options.userdir)
+        assert os.path.exists(userdir), "Given user-defined directory for features doesn't exist."
+    else:
+        raise IOError('One of Freesurfer or user-defined directory must be specified.')
+
+    outdir = os.path.abspath(options.outdir)
     if not os.path.exists(outdir):
         try:
             os.mkdir(outdir)
         except:
             raise
 
-    return fsdir, metadatafile, outdir
+    return metadatafile, outdir, userdir, fsdir
 
 
 def getmetadata(path):
     """Populates the dataset dictionary with subject ids and classes"""
 
 
-def fsvolumes(path):
+def fsvolumes(path, subjid):
     """Returns a feature set of volumes found in aparc+aseg.stats"""
 
 
-def fsthickness(path, fwhm=10):
+def fsthickness(path, subjid, fwhm=10):
     """
     Returns thickness feature set at a given fwhm.
 
@@ -71,7 +95,23 @@ def fsthickness(path, fwhm=10):
     """
 
 
-def getfeatures(subjects, classes, getmethod = None):
+def userdefinedget(featdir, subjid):
+    """
+    Method to read in features for a given subject from a user-defined feature folder. This featdir must contain a
+    separate folder for each subject with a file called features.txt with one number per line.
+
+    :param featdir:
+    :return: vector of numbers.
+    """
+
+    featfile = os.path.join(featdir, subjid, 'features.txt')
+    with open(featfile,'r') as fid:
+        data = fid.read().splitlines()
+
+    return data
+
+
+def getfeatures(subjects, classes, featdir, getmethod = None):
     """Populates the pyradigm data structure with features from a given method.
 
     getmethod: takes in a path and returns a vectorized feature set (e.g. set of subcortical volumes).
@@ -89,9 +129,13 @@ def getfeatures(subjects, classes, getmethod = None):
         class_labels[cls] = idx
 
     ds = MLDataset()
-    for sub in subjects:
-        data = getmethod(sub)
-        ds.add_sample(sub, data, class_labels[classes[sub]], classes[sub])
+    for subjid in subjects:
+        data = getmethod(featdir, subjid)
+        ds.add_sample(subjid, data, class_labels[classes[subjid]], classes[subjid])
+
+    # save the dataset to disk to enable passing on multiple dataset(s)
+    savepath = os.path.join(featdir, 'features_{}.MLDataset'.format(getmethod.__name__))
+    ds.save(savepath)
 
     return ds
 
@@ -112,12 +156,21 @@ def run_rhst(datasets, outdir):
 def run():
     """Main entry point."""
 
-    fsdir, metadatafile, outdir = parse_args()
+    metadatafile, outdir, userdir, fsdir = parse_args()
 
-    subjects = getmetadata(metadatafile)
+    subjects, classes = getmetadata(metadatafile)
 
-    # let's start with one method/featureset for now
-    dataset = getfeatures(subjects, getmethod = 'fsvolumes')
+    # let's start with one method/feature set for now
+    chosenmethod = fsvolumes
+    if not_unspecified(userdir):
+        chosenmethod = lambda dir, sub: userdefinedget(dir, sub)
+    else:
+        chosenmethod = lambda dir, sub: fsvolumes(dir, sub)
+
+    # # this could be a list of methods when RHsT is able to handle it.
+    # chosenmethod = [ fsvolumes, fsthickness, userdefinedget ]
+
+    dataset = getfeatures(subjects, classes, getmethod = chosenmethod)
 
     run_rhst(dataset, outdir)
 
