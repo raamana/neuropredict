a77cbe1a67cb10c201fdba4ee93296744faf2391
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2017-03-28 20:28:03 -0400

    New file created as a reminder to implement nemenyi test.

---
+++ neuropredict/model_comparison.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.stats import friedmanchisquare
+
+
+def vertical_nemenyi_plot(data, num_reps,
+                          alpha = 0.05,
+                          cmap = plt.cm.Greens):
+    """Vertical Nemenyi plot to compare model ranks and show differences.
+
+
+    Take a look at this implementation also:
+    https://gist.github.com/ptasheq/ceb29503fbc16b048bb121684f7fe7dc
+
+    """
+
+
+
+def nemenyi_test():
+    """Nemenyi test.
+
+
+    Take a look at this implementation also:
+    https://gist.github.com/ptasheq/ceb29503fbc16b048bb121684f7fe7dc
+
+    """
