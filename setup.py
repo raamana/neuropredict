7c8b5bb0935739bbd815f75fd0f5fc3be08a954f
Pradeep Reddy Raamana <raamana@gmail.com>
2017-08-11 16:05:07 -0400

    improving the setup and requirements.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name='neuropredict',
-      version='0.2.4.6',
+      version='0.2.4.8',
       description='Neuroimaging Predictive Analysis',
       long_description='Neuroimaging Predictive Analysis; neuropredict',
       author='Pradeep Reddy Raamana',
       author_email='raamana@gmail.com',
       url='https://github.com/raamana/neuropredict',
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]), # ['neuropredict'],
-      install_requires=['numpy', 'scikit-learn', 'pyradigm', 'nibabel'],
+      install_requires=['numpy', 'scikit-learn', 'pyradigm', 'nibabel', 'scipy', 'matplotlib', 'setuptools'],
       classifiers=[
               'Intended Audience :: Science/Research',
               'Programming Language :: Python',
