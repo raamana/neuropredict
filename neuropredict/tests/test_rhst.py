6ac07251c007a874ff7102cf60ae703abecff22e
Pradeep Reddy Raamana <raamana@gmail.com>
2017-09-18 10:16:32 -0400

    fixing # values to unpack in test

--- neuropredict/tests/test_rhst.py
+++ neuropredict/tests/test_rhst.py
@@ -98,8 +98,7 @@ def test_chance_classifier_binary():
 
     dataset_paths, method_names, train_perc, num_repetitions, num_classes, \
         pred_prob_per_class, pred_labels_per_rep_fs, test_labels_per_rep, \
-        best_min_leaf_size, best_num_predictors, \
-        feature_importances_rf, feature_names, \
+        best_params, feature_importances_rf, feature_names, \
         num_times_misclfd, num_times_tested, \
         confusion_matrix, class_set, accuracy_balanced, auc_weighted, positive_class = rhst.load_results(res_path)
 
