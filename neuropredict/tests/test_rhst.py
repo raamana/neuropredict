8bdb92b785fded65d0aeb5f405b2789aa25615b6
Pradeep Reddy Raamana <raamana@gmail.com>
2017-10-29 09:52:20 -0400

    improvign the tests for visualization

--- neuropredict/tests/test_rhst.py
+++ neuropredict/tests/test_rhst.py
@@ -10,6 +10,7 @@ sys.dont_write_bytecode = True
 
 from pyradigm import MLDataset
 import pytest
+from pytest import raises, warns
 
 if __name__ == '__main__' and __package__ is None:
     parent_dir = dirname(dirname(abspath(__file__)))
@@ -68,33 +69,67 @@ def make_random_MLdataset(max_num_classes = 20,
     return ds
 
 
-@pytest.mark.workflow_slow
-def test_chance_classifier_binary():
+max_num_classes = 3
+max_class_size = 40
+max_dim = 50
+num_repetitions =  50
 
-    max_num_classes = 3
-    max_class_size = 40
-    max_dim = 50
-    num_repetitions =  50
+train_perc = 0.8
+red_dim = 'sqrt'
+classifier = 'extratreesclassifier'
+gs_level = 'none'
 
-    # using a really small sample size for faster testing.
-    rand_ds = make_random_MLdataset(max_num_classes=max_num_classes, stratified=True,
-        max_class_size = max_class_size, max_dim = max_dim)
+num_procs = 8
 
-    out_path = os.path.join(out_dir, 'two_classes_random_features.pkl')
-    rand_two_class = rand_ds.get_class(rand_ds.class_set[0:3])
-    rand_two_class.save(out_path)
 
-    rand_ds2 = rand_ds # make_random_MLdataset(max_num_classes=max_num_classes, stratified=True, max_class_size = max_class_size, max_dim = max_dim)
-    out_path2 = os.path.join(out_dir, 'two_classes_random_features_another.pkl')
-    rand_ds2.save(out_path2)
+# using a really small sample size for faster testing.
+rand_ds = make_random_MLdataset(max_num_classes=max_num_classes, stratified=True,
+    max_class_size = max_class_size, max_dim = max_dim)
 
-    out_list = os.path.join(out_dir, 'same_data_two_classes_list_datasets.txt')
-    with open(out_list, 'w') as lf:
-        lf.writelines('\n'.join([out_path, out_path2]))
+out_path = os.path.join(out_dir, 'two_classes_random_features.pkl')
+rand_two_class = rand_ds.get_class(rand_ds.class_set[0:3])
+rand_two_class.save(out_path)
 
-    res_path = rhst.run(out_list, ['random1', 'another'], out_dir,
-                        train_perc=0.5, num_repetitions=num_repetitions,
-                        num_procs=4, grid_search_level='light')
+rand_ds2 = rand_ds # make_random_MLdataset(max_num_classes=max_num_classes, stratified=True, max_class_size = max_class_size, max_dim = max_dim)
+out_path2 = os.path.join(out_dir, 'two_classes_random_features_another.pkl')
+rand_ds2.save(out_path2)
+
+ds_path_list = os.path.join(out_dir, 'same_data_two_classes_list_datasets.txt')
+with open(ds_path_list, 'w') as lf:
+    lf.writelines('\n'.join([out_path, out_path2]))
+
+method_names = ['random1', 'another']
+
+def test_chance_clf_binary_rfc():
+
+    global ds_path_list, method_names, out_dir, num_repetitions, gs_level, train_perc, num_procs
+
+    res_path = rhst.run(ds_path_list, method_names, out_dir,
+                        train_perc=train_perc, num_repetitions=num_repetitions,
+                        num_procs=num_procs, grid_search_level=gs_level)
+
+    dataset_paths, method_names, train_perc, num_repetitions, num_classes, \
+        pred_prob_per_class, pred_labels_per_rep_fs, test_labels_per_rep, \
+        best_params, feature_importances_rf, feature_names, \
+        num_times_misclfd, num_times_tested, \
+        confusion_matrix, class_set, class_sizes, accuracy_balanced, auc_weighted, positive_class = rhst.load_results(res_path)
+
+    median_bal_acc = np.median(accuracy_balanced)
+    # median_wtd_auc = np.median(auc_weighted)
+    chance_acc = rhst.chance_accuracy(class_sizes)
+
+    if abs(median_bal_acc-chance_acc) > chance_acc:
+        raise ValueError('Chance accuracy estimated via repeated holdout CV to substantially differs from that based on class sizes : {}'.format(chance_acc))
+
+
+def test_chance_clf_binary_etc():
+
+    global ds_path_list, method_names, out_dir, num_repetitions, gs_level, train_perc, num_procs
+
+    res_path = rhst.run(ds_path_list, method_names, out_dir,
+                        train_perc=train_perc, num_repetitions=num_repetitions,
+                        num_procs=num_procs, grid_search_level=gs_level,
+                        classifier='extratreesclassifier')
 
     dataset_paths, method_names, train_perc, num_repetitions, num_classes, \
         pred_prob_per_class, pred_labels_per_rep_fs, test_labels_per_rep, \
@@ -113,19 +148,31 @@ def test_chance_classifier_binary():
 def test_versioning():
     " ensures the CLI works. "
 
-    sys.argv = shlex.split('neuropredict -v')
-
-    cli()
+    with raises(SystemExit):
+        sys.argv = shlex.split('neuropredict -v')
+        cli()
 
 def test_vis():
     " ensures the CLI works. "
 
-    sys.argv = shlex.split('neuropredict --make_vis {}'.format(out_dir))
-
-    cli()
+    res_path = pjoin(out_dir, 'rhst_results.pkl')
+    if pexists(res_path):
+        with raises(SystemExit):
+            sys.argv = shlex.split('neuropredict --make_vis {}'.format(out_dir))
+            cli()
+            expected_results = ['balanced_accuracy.pdf', 'compare_misclf_rates.pdf', 'feature_importance.pdf']
+            for rpath in expected_results:
+                if not pexists(rpath):
+                    raise ValueError('an expected result {} not produced'.format(rpath))
+    else:
+        print('previously computed results not found in \n {}'.format(out_dir))
 
 # res_path = pjoin(out_dir, 'rhst_results.pkl')
 # run_workflow.make_visualizations(res_path, out_dir)
-# test_chance_classifier_binary()
+# test_chance_clf_binary_rfc()
+# test_chance_clf_binary_etc()
+
 # test_versioning()
-test_vis()
+# test_vis()
+
+# etc_debug()
