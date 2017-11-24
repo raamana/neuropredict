dcf3e8f1b3b5d032a0480fc95e9fe5876759262d
Pradeep Reddy Raamana <raamana@gmail.com>
2017-11-24 11:01:00 -0500

    finalizing loading of results with multiple subgroups

--- neuropredict/rhst.py
+++ neuropredict/rhst.py
@@ -210,9 +210,11 @@ def load_results_from_folder(results_folder):
 
     results = dict()
     options = load_options(results_folder)
-    for sg  in options['sub_groups']:
+    for sg in options['sub_groups']:
         sg_id = sub_group_identifier(sg)
         results_file_path = pjoin(results_folder, sg_id, cfg.file_name_results)
+        if not pexists(results_file_path) or os.path.getsize(results_file_path) <= 0:
+            raise IOError('Results file for sub group {} does not exist or is empty!'.format(sg_id))
         results[sg_id] = load_results(results_file_path)
 
     return results
