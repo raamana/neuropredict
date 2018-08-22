04591d74e5081f6c52117204b2ee9387ce437700
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2018-08-22 14:23:50 -0400

    using the correct env variable to check num procs under slurm

--- neuropredict/utils.py
+++ neuropredict/utils.py
@@ -195,7 +195,7 @@ def check_num_procs(requested_num_procs=cfg.DEFAULT_NUM_PROCS):
         from os import getenv
 
         hpc_num_procs_spec = [('SGE',   'JOB_ID',      'NSLOTS',        'slots'),
-                              ('SLURM', 'SLURM_JOBID', 'SLURM_NPROCS',  'processors'),
+                              ('SLURM', 'SLURM_JOBID', 'SLURM_CPUS_PER_TASK',  'processors'),
                               ('PBS',   'PBS_JOBID',   'PBS_NUM_PPN',   'processors per node')]
 
         for hpc_env, id_jobid, var_slot_count, var_descr in hpc_num_procs_spec:
