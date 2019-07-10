df7676fb6e356dd77302e291375d23d88409eb77
Pradeep Reddy Raamana <raamana@gmail.com>
2019-07-10 11:52:29 -0400

    generalizing setup for missing data imputation tests

--- neuropredict/tests/test_missing_data.py
+++ neuropredict/tests/test_missing_data.py
@@ -2,19 +2,33 @@ from os.path import abspath, dirname, exists as pexists, join as pjoin, realpath
 import os
 import shlex
 import sys
+import numpy as np
 from neuropredict import cli as run_cli, config_neuropredict as cfg
 from neuropredict.run_workflow import MissingDataException
 from pytest import raises
+from pyradigm import MLDataset
 
-# in_dir = '/Volumes/data/work/rotman/CANBIND/data/Tier1_v1/processing'
-in_dir = '/home/praamana/CANBIND'
-out_dir = pjoin(in_dir, 'missing')
+test_dir = dirname(os.path.realpath(__file__))
+data_dir = realpath(pjoin(test_dir, '..', '..', 'example_datasets'))
+
+out_dir = pjoin(test_dir, 'missing')
 os.makedirs(out_dir, exist_ok=True)
 
-ds_with_missing1 = pjoin(in_dir, 'CANBIND_Tier1_Clinical_features.KeepNaN.MLDataset.pkl')
-ds_with_missing2 = pjoin(in_dir, 'CANBIND_Tier1_Molecular_features.KeepNaN.MLDataset.pkl')
+iris_path = pjoin(data_dir, 'pyradigm', 'iris.MLDataset.pkl')
 
-ds_list = [ds_with_missing1, ds_with_missing2]
+ds_with_missing1 = MLDataset(filepath=iris_path)
+ds_with_missing1['row141'] = [1, 2, np.NaN, 2]
+
+ds_with_missing2 = MLDataset(filepath=iris_path)
+ds_with_missing2['row043'] = [1, np.NaN, 2, 5]
+
+ds_with_missing1_path = pjoin(out_dir, 'trial1.Iris.MLDataset.pkl')
+ds_with_missing2_path = pjoin(out_dir, 'trial2.Iris.MLDataset.pkl')
+
+ds_with_missing1.save(ds_with_missing1_path)
+ds_with_missing2.save(ds_with_missing2_path)
+
+ds_list = [ds_with_missing1_path, ds_with_missing2_path]
 
 
 def test_raises_missing_data_error():
@@ -37,7 +51,8 @@ def test_imputation_works_when_chosen():
     for strategy in cfg.avail_imputation_strategies:
         try:
             sys.argv = shlex.split('neuropredict -n 10 -t 0.5 --impute_strategy {} '
-                                   '-y {} -o {}'.format(strategy, ds_with_missing1, out_dir))
+                                   '-y {} -o {}'.format(strategy,
+                                                        ds_with_missing1_path, out_dir))
             run_cli()
         except:
             raise RuntimeError('Imputation with {} strategy failed!'.format(strategy))
