fe13723a59243b526f38723d962ee25b0eecd210
Pradeep Reddy Raamana <raamana@gmail.com>
2019-11-24 16:04:29 -0500

    improved test with covariates etc [skip ci]

--- neuropredict/tests/test_regress_workflow.py
+++ neuropredict/tests/test_regress_workflow.py
@@ -1,23 +1,21 @@
 import os
+import random
 import shlex
 import sys
 from os.path import abspath, dirname, exists as pexists, join as pjoin, realpath
-from sys import version_info
-import random
+
 import numpy as np
 
 sys.dont_write_bytecode = True
-
-from pytest import raises
 
 if __name__ == '__main__' and __package__ is None:
     parent_dir = dirname(dirname(abspath(__file__)))
     sys.path.append(parent_dir)
 
-from neuropredict import config_neuropredict as cfg
 from neuropredict.regress import cli
 from pyradigm import RegressionDataset
-from pyradigm.utils import make_random_RegrDataset
+from pyradigm.utils import (make_random_RegrDataset,
+                            dataset_with_new_features_same_everything_else)
 
 feat_generator = np.random.randn
 
@@ -28,8 +26,8 @@ out_dir = realpath(pjoin(test_dir, '..', 'tests', 'scratch_regress'))
 if not pexists(out_dir):
     os.makedirs(out_dir)
 
-min_size=300
-max_size=500
+min_size = 300
+max_size = 500
 max_dim = 100
 
 min_num_modalities = 3
@@ -39,35 +37,30 @@ train_perc = 0.5
 num_rep_cv = 20
 red_dim = 'sqrt'
 estimator = 'randomforestregressor'
-dr_method = 'variancethreshold' # 'selectkbest_f_classif'
-gs_level = 'none' # 'light'
+dr_method = 'variancethreshold'  # 'selectkbest_f_classif'
+gs_level = 'none'  # 'light'
 
 num_procs = 1
-
-
-def new_dataset_with_same_ids_targets(in_ds):
-    feat_dim = np.random.randint(1, max_dim)
-    out_ds = RegressionDataset()
-    for id_ in in_ds.samplet_ids:
-        out_ds.add_samplet(id_,  np.random.rand(feat_dim), target=in_ds.targets[id_])
-    return out_ds
 
 out_path = os.path.join(out_dir, 'random_regr_ds1.pkl')
 if pexists(out_path):
     ds_one = RegressionDataset(dataset_path=out_path)
 else:
     ds_one = make_random_RegrDataset(min_size=min_size, max_size=max_size,
-                                     max_dim=max_dim)
+                                     max_dim=max_dim,
+                                     attr_names=('age', 'gender', 'dummy'),
+                                     attr_types=('age', 'gender', 'float')
+                                     )
     ds_one.description = 'ds_one'
     ds_one.save(out_path)
 
 out_path2 = os.path.join(out_dir, 'random_regr_ds2.pkl')
-ds_two = new_dataset_with_same_ids_targets(ds_one)
+ds_two = dataset_with_new_features_same_everything_else(ds_one, max_dim)
 ds_two.description = 'ds_two'
 ds_two.save(out_path2)
 
 sys.argv = shlex.split('np_regress -y {} {} -t {} -n {} -c {} -g {} -o {} '
-                       '-e {} -dr {}'
+                       '-e {} -dr {} -cl age -cm residualize'
                        ''.format(out_path, out_path2, train_perc, num_rep_cv,
                                  num_procs, gs_level, out_dir, estimator, dr_method))
 cli()
