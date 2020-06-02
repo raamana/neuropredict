ad5d1d992879352787faa5396fb7584bc98f00fa
Pradeep Reddy Raamana <raamana@gmail.com>
2020-06-02 18:02:03 -0400

    updating tests and related for imputation functionality

--- neuropredict/tests/test_missing_data.py
+++ neuropredict/tests/test_missing_data.py
@@ -1,13 +1,14 @@
-from os.path import abspath, dirname, exists as pexists, join as pjoin, realpath
 import os
 import shlex
 import sys
+from os.path import dirname, join as pjoin, realpath
+
 import numpy as np
-from neuropredict.classify import cli as run_cli
 from neuropredict import config as cfg
 from neuropredict.base import MissingDataException
-from pytest import raises
+from neuropredict.classify import cli as run_cli
 from pyradigm import ClassificationDataset
+from pytest import raises
 
 test_dir = dirname(os.path.realpath(__file__))
 data_dir = realpath(pjoin(test_dir, '..', '..', 'example_datasets'))
@@ -17,10 +18,12 @@ os.makedirs(out_dir, exist_ok=True)
 
 iris_path = pjoin(data_dir, 'pyradigm', 'iris.MLDataset.pkl')
 
-ds_with_missing1 = ClassificationDataset(dataset_path=iris_path)
+ds_with_missing1 = ClassificationDataset(dataset_path=iris_path,
+                                         allow_nan_inf=True)
 ds_with_missing1['row141'] = [1, 2, np.NaN, 2]
 
-ds_with_missing2 = ClassificationDataset(dataset_path=iris_path)
+ds_with_missing2 = ClassificationDataset(dataset_path=iris_path,
+                                         allow_nan_inf=True)
 ds_with_missing2['row043'] = [1, np.NaN, 2, 5]
 
 ds_path_missing1 = pjoin(out_dir, 'trial1.Iris.MLDataset.pkl')
@@ -59,5 +62,5 @@ def test_imputation_works_when_chosen():
                                    ''.format(strategy, ds_path_missing1, out_dir))
             run_cli()
         except:
-            raise RuntimeError('Imputation with {} strategy failed!'.format(strategy))
-
+            raise RuntimeError(
+                'Imputation with {} strategy failed!'.format(strategy))
