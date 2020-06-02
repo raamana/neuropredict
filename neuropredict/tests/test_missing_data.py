e3d497f3c9921198f2f2adfca48ce4137ed7f30b
Pradeep Reddy Raamana <raamana@gmail.com>
2020-06-02 17:47:44 -0400

    Not done: updating impute tests with latest data structures

--- neuropredict/tests/test_missing_data.py
+++ neuropredict/tests/test_missing_data.py
@@ -3,10 +3,11 @@ import os
 import shlex
 import sys
 import numpy as np
-from neuropredict import cli as run_cli, config as cfg
+from neuropredict.classify import cli as run_cli
+from neuropredict import config as cfg
 from neuropredict.base import MissingDataException
 from pytest import raises
-from pyradigm import MLDataset
+from pyradigm import ClassificationDataset
 
 test_dir = dirname(os.path.realpath(__file__))
 data_dir = realpath(pjoin(test_dir, '..', '..', 'example_datasets'))
@@ -16,19 +17,19 @@ os.makedirs(out_dir, exist_ok=True)
 
 iris_path = pjoin(data_dir, 'pyradigm', 'iris.MLDataset.pkl')
 
-ds_with_missing1 = MLDataset(filepath=iris_path)
+ds_with_missing1 = ClassificationDataset(dataset_path=iris_path)
 ds_with_missing1['row141'] = [1, 2, np.NaN, 2]
 
-ds_with_missing2 = MLDataset(filepath=iris_path)
+ds_with_missing2 = ClassificationDataset(dataset_path=iris_path)
 ds_with_missing2['row043'] = [1, np.NaN, 2, 5]
 
-ds_with_missing1_path = pjoin(out_dir, 'trial1.Iris.MLDataset.pkl')
-ds_with_missing2_path = pjoin(out_dir, 'trial2.Iris.MLDataset.pkl')
+ds_path_missing1 = pjoin(out_dir, 'trial1.Iris.MLDataset.pkl')
+ds_path_missing2 = pjoin(out_dir, 'trial2.Iris.MLDataset.pkl')
 
-ds_with_missing1.save(ds_with_missing1_path)
-ds_with_missing2.save(ds_with_missing2_path)
+ds_with_missing1.save(ds_path_missing1)
+ds_with_missing2.save(ds_path_missing2)
 
-ds_list = [ds_with_missing1_path, ds_with_missing2_path]
+ds_list = [ds_path_missing1, ds_path_missing2]
 
 
 def test_raises_missing_data_error():
@@ -37,12 +38,14 @@ def test_raises_missing_data_error():
     for ds_path in ds_list:
         # default is to raise, even if not specified
         with raises((MissingDataException,)):
-            sys.argv = shlex.split('neuropredict -e svm -y {} -o {}'.format(ds_path, out_dir))
+            sys.argv = shlex.split('neuropredict -e svm -y {} -o {}'
+                                   ''.format(ds_path, out_dir))
             run_cli()
 
         # checking explicit raise
         with raises((MissingDataException,)):
-            sys.argv = shlex.split('neuropredict -y {} -o {} --impute_strategy raise '
+            sys.argv = shlex.split('neuropredict -y {} -o {} '
+                                   ' --impute_strategy raise '
                                    ''.format(ds_path, out_dir))
             run_cli()
 
@@ -50,9 +53,10 @@ def test_raises_missing_data_error():
 def test_imputation_works_when_chosen():
     for strategy in cfg.avail_imputation_strategies:
         try:
-            sys.argv = shlex.split('neuropredict -n 10 -t 0.5 --impute_strategy {} '
-                                   '-y {} -o {}'.format(strategy,
-                                                        ds_with_missing1_path, out_dir))
+            sys.argv = shlex.split('neuropredict -n 10 -t 0.5 '
+                                   ' --impute_strategy {} '
+                                   '-y {} -o {}'
+                                   ''.format(strategy, ds_path_missing1, out_dir))
             run_cli()
         except:
             raise RuntimeError('Imputation with {} strategy failed!'.format(strategy))
