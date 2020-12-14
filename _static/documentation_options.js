4cdb4c2f2da51b55937a9ecd81caf7dd7b047222
Pradeep Reddy Raamana <raamana@gmail.com>
2020-12-14 12:28:33 -0500

    updated html

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6+2.gb0e022b',
+    VERSION: '0.6+3.g3e6b884.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
